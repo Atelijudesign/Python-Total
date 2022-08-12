b8818f845e4db17f3cdf002215130b633dd61c57
atelijudesign <atelijudesign@gmail.com>
2022-08-11 22:49:57 -0400

    Add script day 1

---
+++ Dia01_Creador Nombre.py
@@ -0,0 +1,39 @@
+# #Linea de comentario
+# print("Hola Mundo")
+#
+# #Imprimir con salto de linea
+# print('Este texto esta dividido \n en dos lineas')
+#
+# #Ejercicio 1
+# print("Me encanta estudiar Python")
+#
+# #Ejercicio 2
+# print('Estudiar con "Python Total" es super divertido')
+#
+# #Ejercicio 3
+# print(5+550)
+#
+# #Ejercicio 4
+# print("Línea 1\nLínea 2\nLínea 3")
+#
+# #ejercicio 5
+# print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+#
+# #Ejercicio 6
+# print("Barra Normal '/' \n Barra Invertida \\")
+#
+# #Ejercicio 7
+# print(input("¿Qué estás estudiando?"))
+#
+# #Ejercicio 8
+# print(input("¿En qué país vives?"))
+#
+# #Ejercicio 9
+# print(input("Escribe tu nombre:")+" "+input("Escribe tu apellido:"))
+#
+# #Input es una funcion de Python
+#
+# print("Tu nombre es: " + input("Dime tu nombre:")+" "+input("Dime tu Apellido:"))
+
+#Proyecto del 1
+print("El nombre de tu cerveza\nes '"+ input("Que ciudad te gustaria visitar?: ")+ " "+ input("Cual es tu color favorito?: ")+ "'\nFelicitaciones!!")
