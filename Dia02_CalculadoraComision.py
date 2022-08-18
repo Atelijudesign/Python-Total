b941ea7bdae8983f14be79e5d17dbe338c59177c
atelijudesign <atelijudesign@gmail.com>
2022-08-17 23:36:24 -0400

    dia 2

--- Dia02_CalculadoraComision.py
+++ Dia02_CalculadoraComision.py
@@ -46,13 +46,45 @@
 # print(nueva_edad)
 
 #Formatear Cadenas
-x = 6
-y = 7
-print("Mis numeros son: " +str(x)+  " y " +str(y))
-print("Mis numeros son: {} y {}".format(x,y))
-print("La suma de : {} y {} es igual a {}".format(x,y, x+y))
+# x = 6
+# y = 7
+# print("Mis numeros son: " +str(x)+  " y " +str(y))
+# print("Mis numeros son: {} y {}".format(x,y))
+# print("La suma de : {} y {} es igual a {}".format(x,y, x+y))
 
 #Cademas Literales
-color = "Rojo"
-matricula = "'RBSG25'"
-print(f"El auto es de color {color} y su matricula es {matricula}")
+# color = "Rojo"
+# matricula = "'RBSG25'"
+# print(f"El auto es de color {color} y su matricula es {matricula}")
+
+#Operaciones matematicas
+# print("---------------------")
+# x=6
+# y= 5
+# z = 7
+# print(f"La suma de {x} mas {y} es igual a {x+y}")
+# print(f"La multiplicacion  de {x} por {y} es igual a {x*y}")
+# print(f"La division  de {x} dividido {y} es igual a {x/y}")
+# print(f"La resta de {x} menos {y} es igual a {x-y}")
+# print(f"El modulo  de {z} mas {y} es igual a {z%y}")
+# print(f" {x} elevado a {y} es igual a {x**y}")
+
+# print("Redondeo")
+# x = 9.6568
+# y = 205
+# z = x*y
+# print(round(z,2))
+# num1 = round(13/2,0)
+# print(num1)
+# print(type(num1))
+#Desafio día 2
+print("Desafio día 2")
+nombre = input("Por favor ingrese su nombre: ")
+ventas = int(input("Indique sus ventas totales del mes: "))
+
+#Formula para calcular la comision
+comision = round(ventas*13/100,2)
+
+ventaTotal = ventas+comision
+
+print(f"Hola {nombre}, tus comision  de este mes fue: ${comision}. Tu total a pagar es: ${ventaTotal}")
