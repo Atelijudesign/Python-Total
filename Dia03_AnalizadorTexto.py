1b8ea4a675d18108854f00cbed6c265c0bacfa81
Andres Gallo Parra <53225670+Atelijudesign@users.noreply.github.com>
2022-09-12 13:44:31 -0300

    "Avance dia 3"

---
+++ Dia03_AnalizadorTexto.py
@@ -0,0 +1,12 @@
+#Index()
+#Extraer sub-strings
+#Métodos y propiedades de string
+#Listas
+#Diccionarios
+#Tuples
+#Sets
+
+mi_texto = "Esta es una prueba"
+resultado = mi_texto.rindex("E")
+
+print(resultado)
